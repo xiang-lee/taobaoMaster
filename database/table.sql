66aebc36824deef566ee697ee91203c2a890fd8c
xiangluffy <xiangireland@gmail.com>
2015-03-07 18:58:37 +0000

    Add create table sql

--- database/table.sql
+++ database/table.sql
@@ -7,3 +7,48 @@ CREATE TABLE `tm_user` (
   PRIMARY KEY (`id`)
 );
 
+
+CREATE TABLE `tm_product` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `record_date` DATE  NOT NULL DEFAULT '0000-00-00',
+  `quantity` smallint(4) NOT NULL DEFAULT 0,
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `unit_price` DECIMAL(8,2) NOT NULL DEFAULT 0.00,
+  `currency` varchar(10) NOT NULL DEFAULT 'euro',
+  `exchange_rate` DECIMAL(5,4) NOT NULL DEFAULT '0.0000',
+  `order_number` varchar(100) NOT NULL DEFAULT '',
+   PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `tm_buying` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `payer` varchar(50) NOT NULL DEFAULT '',
+  `arrive_date` DATE  NOT NULL DEFAULT '0000-00-00',
+  `remain` smallint(4) NOT NULL DEFAULT 0,
+  `is_stockpile` tinyint(1) NOT NULL DEFAULT 0,
+  `product_id` bigint(4) unsigned NOT NULL,
+   FOREIGN KEY (`product_id`) REFERENCES `tm_product` (`id`),
+   PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `tm_selling` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sell_unit_price` DECIMAL(8,2) NOT NULL DEFAULT 0.00,
+  `postage` DECIMAL(6,2) NOT NULL DEFAULT 0.00,
+  `sell_currency` varchar(10) NOT NULL DEFAULT 'cny',
+  `stockpile_id` bigint(20) unsigned DEFAULT 0,
+  `reduced_price` DECIMAL(8,2) NOT NULL DEFAULT 0.00,
+  `is_received` tinyint(1) NOT NULL DEFAULT 0,
+  `is_refund` tinyint(1) NOT NULL DEFAULT 0,
+  `is_sold_to_friend` tinyint(1) NOT NULL DEFAULT 0,
+  `is_brush` tinyint(1) NOT NULL DEFAULT 0,
+  `is_repay_brush_buyer` tinyint(1) NOT NULL DEFAULT 0,
+  `buyer` varchar(50) NOT NULL DEFAULT '',
+  `deliver_date` DATE  NOT NULL DEFAULT '0000-00-00',
+  `product_id` bigint(4) unsigned NOT NULL,
+   FOREIGN KEY (`product_id`) REFERENCES `tm_product` (`id`),
+   PRIMARY KEY (`id`)
+);
